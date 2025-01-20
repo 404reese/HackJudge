b82f2a75d657b01b0fb547dd6bc1a13ced57c9af
Riddhesh <riddhesh.chaudhary@gmail.com>
2025-01-20 22:29:37 +0530

    font-config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
-/** @type {import('tailwindcss').Config} */
-export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}", // Make sure you add paths to your components
+    "./public/index.html",
+  ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        minecraft: ["Minecraft", "sans-serif"], // Custom font name
+      },
+    },
   },
   plugins: [],
-};
+}
